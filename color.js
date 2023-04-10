f0d3b5036ab1b09c49fb3b4b1d5d81009b61dd1a
BeqaGvelesiani <107481738+BeqaGvelesiani@users.noreply.github.com>
2023-04-10 13:09:45 +0400

    Add files via upload

---
+++ color.js
@@ -0,0 +1,37 @@
+const pallazo150 = document.getElementById("pallazo-150");
+const siena50 = document.getElementById("siena-50");
+const melise90 = document.getElementById("melise-90");
+const palazzo115 = document.getElementById("palazzo-115");
+const marill60 = document.getElementById("marill-60");
+const Ginster55 = document.getElementById("Ginster-55");
+const pallazo55 = document.getElementById("pallazo-55");
+
+const root = document.querySelector(":root");
+
+pallazo150.addEventListener("click", () =>
+  root.style.setProperty("--Bcolor", "#e0ceb6")
+);
+
+siena50.addEventListener("click", () =>
+  root.style.setProperty("--Bcolor", "#d9c9bd")
+);
+
+melise90.addEventListener("click", () =>
+  root.style.setProperty("--Bcolor", "#faf0d3")
+);
+
+palazzo115.addEventListener("click", () =>
+  root.style.setProperty("--Bcolor", "#d8cdc1")
+);
+
+marill60.addEventListener("click", () =>
+  root.style.setProperty("--Bcolor", "#e8d9c9")
+);
+
+Ginster55.addEventListener("click", () =>
+  root.style.setProperty("--Bcolor", "#e3d5be")
+);
+
+pallazo55.addEventListener("click", () =>
+  root.style.setProperty("--Bcolor", "#e2d7cd")
+);
